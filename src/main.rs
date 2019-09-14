99bae937845bbd7c3318af7e5d4c8d2396509145
qiujiangkun <qiujiangkun@foxmail.com>
2019-09-14 20:10:31 +0800

    clean up

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,18 @@ fn main() {
     let matches = App::new("Simple Calculator")
         .version("0.1")
         .author("Jack Quinn")
-        .about("A simple calculator but support complex logic")
+        .about("A simple calculator but supporting complex logic")
         .args_from_usage("-i, --input=[FILE] 'source code file'")
-        .args_from_usage("-o, --output=[FILE] 'output file'")
+//        .args_from_usage("-o, --output=[FILE] 'output file'")
         .get_matches();
 
-    if let Some(f) = matches.value_of("path") {
-        println!("path : {}", f);
-    }
-    let buf = String::from("a = 30;while a { a = a - 1;print(a);}");
-//    let mut buf = String::new();
-//    let mut file = File::open(matches.value_of("input").unwrap_or("/dev/stdin")).expect("Cannot open file");
-//    file.read_to_string(&mut buf).expect("Cannot read file");
+//    let buf = String::from("fn foo(){return 6;} a = foo();while a { a = a - 1;print(a);}");
+    let mut buf = String::new();
+    let mut file = File::open(matches.value_of("input").unwrap_or("/dev/stdin")).expect("Cannot open file");
+    file.read_to_string(&mut buf).expect("Cannot read file");
     let v = parser::parse(&buf, 0);
     // todo log system
-    println!("{:#?}", v);
+    debug!("{:#?}", v);
     runtime::run_code(&v);
 
 }
